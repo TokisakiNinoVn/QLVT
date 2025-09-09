c986e04019e8cd007a8197cdad0afefecb22e016
Tokisaki Nino <trieuduytancbg@gmail.com>
2025-09-09 11:28:11 +0700

    update: add file - gitignore

---
+++ lib/config/app_config.dart
@@ -0,0 +1,8 @@
+// lib/config/app_config.dart
+
+class AppConfig {
+  static const String apiUrl = "https://truyxuatnguongoc.chethainguyen.net.vn/api/v1";
+  static const String apiUrlImage = "https://truyxuatnguongoc.chethainguyen.net.vn/storage";
+  static const String apiKey = "";
+  static const bool isProduction = false;
+}
